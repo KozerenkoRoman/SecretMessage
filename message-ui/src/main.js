82e9acf0542f092bab76008312c680a867ef5ecd
Roman Kozerenko <kozerenko@gmail.com>
2026-06-14 17:31:38 +0300

    add application version

--- message-ui/src/main.js
+++ message-ui/src/main.js
@@ -26,3 +26,10 @@ app.use(router)
 app.use(i18n)
 
 app.mount('#app')
+
+// Вивід версії з package.json в консоль
+console.log(
+    `%c🚀 MESSAGE UI v${__APP_VERSION__} %c Production`,
+    'background: #1e1e2f; color: #00ffcc; padding: 3px 5px; border-radius: 3px; font-weight: bold;',
+    'color: #888;'
+);
